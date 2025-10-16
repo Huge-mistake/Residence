fbe4be76816c9e8b781c166eda33f0759cd60685
Max <137968615+Huge-mistake@users.noreply.github.com>
2025-10-17 02:57:10 +0800

    Update ResidencePlayerListener.java

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener.java
@@ -1451,24 +1451,12 @@ public class ResidencePlayerListener implements Listener {
 
             if (heldItem.isDye() || heldItem.equals(CMIMaterial.GLOW_INK_SAC)) {
 
-                if (heldItem.equals(CMIMaterial.BONE_MEAL) &&
-                    (blockM == CMIMaterial.GRASS_BLOCK ||
-                        blockM == CMIMaterial.GRASS ||
-                        blockM == CMIMaterial.SHORT_GRASS ||
-                        blockM == CMIMaterial.TALL_GRASS ||
-                        blockM == CMIMaterial.TALL_SEAGRASS ||
-                        blockM == CMIMaterial.MOSS_BLOCK ||
-                        blockM == CMIMaterial.BIG_DRIPLEAF_STEM ||
-                        blockM == CMIMaterial.BIG_DRIPLEAF ||
-                        blockM == CMIMaterial.SMALL_DRIPLEAF ||
-                        blockM.isSapling()) ||
-                    heldItem == CMIMaterial.COCOA_BEANS && blockM == CMIMaterial.JUNGLE_WOOD) {
-                    FlagPermissions tperms = FlagPermissions.getPerms(block.getRelative(event.getBlockFace()).getLocation(), player);
-                    if (!tperms.playerHas(player, Flags.build, true)) {
-                        lm.Flag_Deny.sendMessage(player, Flags.build);
-                        event.setCancelled(true);
-                        return;
-                    }
+            if (heldItem.equals(CMIMaterial.BONE_MEAL)) {
+                FlagPermissions tperms = FlagPermissions.getPerms(block.getRelative(event.getBlockFace()).getLocation(), player);
+                if (!tperms.playerHas(player, Flags.build, true)) {
+                    lm.Flag_Deny.sendMessage(player, Flags.build);
+                    event.setCancelled(true);
+                    return;
                 }
             }
             if (heldItem.equals(CMIMaterial.ARMOR_STAND) || heldItem.isBoat()) {
