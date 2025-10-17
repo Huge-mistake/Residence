fca690b58e573aa32bba6a9eb7b16e282fcf6edb
Max <137968615+Huge-mistake@users.noreply.github.com>
2025-10-17 18:52:43 +0800

    Update FlagPermissions.java

--- src/main/java/com/bekvon/bukkit/residence/protection/FlagPermissions.java
+++ src/main/java/com/bekvon/bukkit/residence/protection/FlagPermissions.java
@@ -233,6 +233,9 @@ public class FlagPermissions {
             if (one.isTrapDoor())
                 matUseFlagList.put(one.getMaterial(), Flags.door);
 
+            if (one.isChest())
+                matUseFlagList.put(one.getMaterial(), Flags.container);
+
             if (one.isShulkerBox())
                 matUseFlagList.put(one.getMaterial(), Flags.container);
 
@@ -264,9 +267,6 @@ public class FlagPermissions {
             }
 
             if (Version.isCurrentEqualOrHigher(Version.v1_21_R6)) {
-                if (one.containsCriteria(CMIMC.COPPERCHEST))
-                    addMaterialToUseFlag(one.getMaterial(), Flags.container);
-
                 if (one.containsCriteria(CMIMC.SHELF))
                     addMaterialToUseFlag(one.getMaterial(), Flags.container);
             }
