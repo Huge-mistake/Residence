d5567dea8abc4ab547012296bc35438f3fd7a494
Max <137968615+Huge-mistake@users.noreply.github.com>
2025-10-22 17:19:44 +0800

    Update ResidenceListener1_16.java

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_16.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_16.java
@@ -2,11 +2,14 @@ package com.bekvon.bukkit.residence.listeners;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.weather.LightningStrikeEvent;
 import org.bukkit.inventory.EquipmentSlot;
@@ -16,6 +19,7 @@ import com.bekvon.bukkit.residence.containers.Flags;
 import com.bekvon.bukkit.residence.containers.ResAdmin;
 import com.bekvon.bukkit.residence.containers.lm;
 import com.bekvon.bukkit.residence.protection.ClaimedResidence;
+import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import com.bekvon.bukkit.residence.protection.FlagPermissions.FlagCombo;
 
 import net.Zrips.CMILib.Version.Version;
@@ -94,5 +98,35 @@ public class ResidenceListener1_16 implements Listener {
                 event.setCancelled(true);
             }
         }
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onFishingBobberHit(ProjectileHitEvent event) {
+        // Anti NPE
+        Entity HitEntity = event.getHitEntity();
+        if (HitEntity == null)
+            return;
+        // disabling event on world
+        if (plugin.isDisabledWorldListener(HitEntity.getWorld()))
+            return;
+
+        Projectile hook = event.getEntity();
+        if (!hook.getClass().getSimpleName().equals("CraftFishingBobber"))
+            return;
+
+        if (!(hook.getShooter() instanceof Player))
+            return;
+
+        Player player = (Player) hook.getShooter();
+
+        if (ResAdmin.isResAdmin(player))
+            return;
+
+        FlagPermissions perms = FlagPermissions.getPerms(HitEntity.getLocation(), player);
+        if (perms.playerHas(player, Flags.hook, true))
+            return;
+
+        lm.Flag_Deny.sendMessage(player, Flags.hook);
+        event.setCancelled(true);
     }
 }
