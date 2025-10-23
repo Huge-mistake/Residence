4b88af8bfaef967d273356c523d17ae682bad933
Max <137968615+Huge-mistake@users.noreply.github.com>
2025-10-23 21:11:28 +0800

    Update ResidenceListener1_19.java

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_19.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_19.java
@@ -1,3 +1,94 @@
+package com.bekvon.bukkit.residence.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Hopper;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+import com.bekvon.bukkit.residence.Residence;
+import com.bekvon.bukkit.residence.containers.Flags;
+import com.bekvon.bukkit.residence.containers.lm;
+import com.bekvon.bukkit.residence.protection.ClaimedResidence;
+import com.bekvon.bukkit.residence.protection.FlagPermissions;
+import com.bekvon.bukkit.residence.protection.FlagPermissions.FlagCombo;
+
+public class ResidenceListener1_19 implements Listener {
+
+    private Residence plugin;
+
+    public ResidenceListener1_19(Residence plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onSignInteract(PlayerInteractEvent event) {
+
+        if (!Flags.goathorn.isGlobalyEnabled())
+            return;
+
+        if (event.getPlayer() == null)
+            return;
+        // disabling event on world
+        if (plugin.isDisabledWorldListener(event.getPlayer().getWorld()))
+            return;
+
+        Player player = event.getPlayer();
+        if (player.hasMetadata("NPC"))
+            return;
+
+        ItemStack horn = event.getItem();
+
+        if (horn == null)
+            return;
+
+        if (!horn.getType().equals(Material.GOAT_HORN))
+            return;
+
+        ClaimedResidence res = ClaimedResidence.getByLoc(event.getPlayer().getLocation());
+        if (res == null)
+            return;
+
+        if (event.getPlayer().hasMetadata("NPC"))
+            return;
+
+        if (res.getPermissions().playerHas(event.getPlayer(), Flags.goathorn, FlagCombo.TrueOrNone))
+            return;
+
+        event.setCancelled(true);
+
+        lm.Residence_FlagDeny.sendMessage(player, Flags.goathorn, res.getName());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onBlockSpread(BlockSpreadEvent event) {
+        // Disabling listener if flag disabled globally
+        if (!Flags.skulk.isGlobalyEnabled())
+            return;
+        // disabling event on world
+        if (plugin.isDisabledWorldListener(event.getBlock().getWorld()))
+            return;
+
+        if (!Material.SCULK_CATALYST.equals(event.getSource().getType()))
+            return;
+
+        Location loc = event.getBlock().getLocation();
+        FlagPermissions perms = FlagPermissions.getPerms(loc);
+        if (!perms.has(Flags.skulk, true)) {
+            event.setCancelled(true);
+        }
+    }
+
     private void breakActor(InventoryHolder actor) {
 
         if (actor == null) {
@@ -70,3 +161,4 @@
         }
         // ignore source & dest not in Res
     }
+}
