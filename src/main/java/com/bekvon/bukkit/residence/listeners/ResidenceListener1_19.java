152d9da8752cd40efd3864fb6caee8f7e8e2f6f1
Max <137968615+Huge-mistake@users.noreply.github.com>
2025-10-24 18:03:37 +0800

    Update ResidenceListener1_19.java

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_19.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_19.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.Hopper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -95,7 +96,7 @@ public class ResidenceListener1_19 implements Listener {
                 return;
             Block block = hopperLoc.getBlock();
             // only hopper
-            if (block.getType() != Material.HOPPER)
+            if (block == null || !(block instanceof Hopper))
                 return;
             block.breakNaturally();
         }, 1);
