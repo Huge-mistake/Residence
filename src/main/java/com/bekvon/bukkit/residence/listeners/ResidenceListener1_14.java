79fb7d0fad015837d1a7588cc34f8a9d88af638b
Max <137968615+Huge-mistake@users.noreply.github.com>
2026-01-20 23:20:31 +0800

    Update ResidenceListener1_14.java

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_14.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_14.java
@@ -3,6 +3,7 @@ package com.bekvon.bukkit.residence.listeners;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -13,9 +14,12 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerTakeLecternBookEvent;
 import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.Flags;
@@ -26,6 +30,7 @@ import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import com.bekvon.bukkit.residence.protection.FlagPermissions.FlagCombo;
 import com.bekvon.bukkit.residence.utils.Utils;
 
+import net.Zrips.CMILib.Entities.CMIEntityType;
 import net.Zrips.CMILib.Logs.CMIDebug;
 import net.Zrips.CMILib.Items.CMIMaterial;
 
@@ -205,5 +210,153 @@ public class ResidenceListener1_14 implements Listener {
         if (FlagPermissions.has(block.getLocation(), Flags.coraldryup, FlagCombo.OnlyFalse)) {
             event.setCancelled(true);
         }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onAnimalFeeding(PlayerInteractEntityEvent event) {
+        // Disabling listener if flag disabled globally
+        if (!Flags.animalfeeding.isGlobalyEnabled())
+            return;
+
+        Entity entity = event.getRightClicked();
+        // disabling event on world
+        if (plugin.isDisabledWorldListener(entity.getWorld()))
+            return;
+
+        if (!(entity instanceof Animals))
+            return;
+
+        Player player = event.getPlayer();
+
+        ItemStack item = event.getHand() == EquipmentSlot.OFF_HAND
+                ? player.getInventory().getItemInOffHand()
+                : player.getInventory().getItemInMainHand();
+
+        if (item == null || item.getType() == Material.AIR)
+            return;
+
+        CMIEntityType type = CMIEntityType.get(entity.getType());
+        Material held = item.getType();
+
+        if (isFeedingAnimal(type, held)) {
+
+            if (ResAdmin.isResAdmin(player))
+                return;
+
+            FlagPermissions perms = FlagPermissions.getPerms(entity.getLocation(), player);
+            if (perms.playerHas(player, Flags.animalfeeding, perms.playerHas(player, Flags.animalkilling, true)))
+                return;
+
+            lm.Flag_Deny.sendMessage(player, Flags.animalfeeding);
+            event.setCancelled(true);
+
+        }
+    }
+
+    private boolean isFeedingAnimal(CMIEntityType type, Material held) {
+        try {
+            switch (type) {
+                case ARMADILLO:
+                    if (Tag.ITEMS_ARMADILLO_FOOD.isTagged(held)) return true;
+                    break;
+                case AXOLOTL:
+                    if (Tag.ITEMS_AXOLOTL_FOOD.isTagged(held)) return true;
+                    break;
+                case BEE:
+                    if (Tag.ITEMS_BEE_FOOD.isTagged(held)) return true;
+                    break;
+                case CAMEL:
+                    if (Tag.ITEMS_CAMEL_FOOD.isTagged(held)) return true;
+                    break;
+                case CAMEL_HUSK:
+                    if (Tag.ITEMS_CAMEL_HUSK_FOOD.isTagged(held)) return true;
+                    break;
+                case CAT:
+                    if (Tag.ITEMS_CAT_FOOD.isTagged(held)) return true;
+                    break;
+                case CHICKEN:
+                    if (Tag.ITEMS_CHICKEN_FOOD.isTagged(held)) return true;
+                    break;
+                case COW:
+                    if (Tag.ITEMS_COW_FOOD.isTagged(held)) return true;
+                    break;
+                case DONKEY:
+                    if (Tag.ITEMS_HORSE_FOOD.isTagged(held)) return true;
+                    break;
+                case FOX:
+                    if (Tag.ITEMS_FOX_FOOD.isTagged(held)) return true;
+                    break;
+                case FROG:
+                    if (Tag.ITEMS_FROG_FOOD.isTagged(held)) return true;
+                    break;
+                case GOAT:
+                    if (Tag.ITEMS_GOAT_FOOD.isTagged(held)) return true;
+                    break;
+                case HAPPY_GHAST:
+                    if (Tag.ITEMS_HAPPY_GHAST_FOOD.isTagged(held)) return true;
+                    break;
+                case HOGLIN:
+                    if (Tag.ITEMS_HOGLIN_FOOD.isTagged(held)) return true;
+                    break;
+                case HORSE:
+                    if (Tag.ITEMS_HORSE_FOOD.isTagged(held)) return true;
+                    break;
+                case LLAMA:
+                    if (Tag.ITEMS_LLAMA_FOOD.isTagged(held)) return true;
+                    break;
+                case MOOSHROOM:
+                    if (Tag.ITEMS_COW_FOOD.isTagged(held)) return true;
+                    break;
+                case MULE:
+                    if (Tag.ITEMS_HORSE_FOOD.isTagged(held)) return true;
+                    break;
+                case NAUTILUS:
+                    if (Tag.ITEMS_NAUTILUS_FOOD.isTagged(held)) return true;
+                    break;
+                case OCELOT:
+                    if (Tag.ITEMS_OCELOT_FOOD.isTagged(held)) return true;
+                    break;
+                case PANDA:
+                    if (Tag.ITEMS_PANDA_FOOD.isTagged(held)) return true;
+                    break;
+                case PARROT:
+                    if (Tag.ITEMS_PARROT_FOOD.isTagged(held)) return true;
+                    break;
+                case PIG:
+                    if (Tag.ITEMS_PIG_FOOD.isTagged(held)) return true;
+                    break;
+                case RABBIT:
+                    if (Tag.ITEMS_RABBIT_FOOD.isTagged(held)) return true;
+                    break;
+                case SHEEP:
+                    if (Tag.ITEMS_SHEEP_FOOD.isTagged(held)) return true;
+                    break;
+                case SNIFFER:
+                    if (Tag.ITEMS_SNIFFER_FOOD.isTagged(held)) return true;
+                    break;
+                case STRIDER:
+                    if (Tag.ITEMS_STRIDER_FOOD.isTagged(held)) return true;
+                    break;
+                case TRADER_LLAMA:
+                    if (Tag.ITEMS_LLAMA_FOOD.isTagged(held)) return true;
+                    break;
+                case TURTLE:
+                    if (Tag.ITEMS_TURTLE_FOOD.isTagged(held)) return true;
+                    break;
+                case WOLF:
+                    if (Tag.ITEMS_WOLF_FOOD.isTagged(held)) return true;
+                    break;
+                case ZOMBIE_HORSE:
+                    if (held == Material.RED_MUSHROOM) return true;
+                    break;
+                case ZOMBIE_NAUTILUS:
+                    if (Tag.ITEMS_NAUTILUS_FOOD.isTagged(held)) return true;
+                    break;
+                default:
+                    break;
+            }
+        } catch (Throwable e) {
+        }
+        return false;
     }
 }
