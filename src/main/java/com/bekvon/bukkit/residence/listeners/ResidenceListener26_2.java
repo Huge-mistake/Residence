b0c9503c3979c3b9541f34986d1521bb5169c17a
CatTeaA <1615856447@qq.com>
2026-08-09 05:14:54 +0800

    fix

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener26_2.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener26_2.java
@@ -99,9 +99,4 @@ public class ResidenceListener26_2 implements Listener {
         event.setCancelled(true);
 
     }
-
-    @EventHandler(ignoreCancelled = true)
-    public void onTest(PlayerInteractEvent event) {
-        event.setCancelled(true);
-    }
 }
