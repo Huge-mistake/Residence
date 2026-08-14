f3be7616243f4a56ff271d1b3ee987d41078b9a3
CatTeaA <1615856447@qq.com>
2026-08-14 13:33:33 +0800

    fix

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceEntityListener.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceEntityListener.java
@@ -931,15 +931,6 @@ public class ResidenceEntityListener implements Listener {
             return;
 
         switch (type) {
-        case ENDER_CRYSTAL:
-            // Disabling listener if flag disabled globally
-            if (!Flags.explode.isGlobalyEnabled())
-                break;
-            if (!perms.has(Flags.explode, perms.has(Flags.destroy, true))) {
-                event.setCancelled(true);
-                ent.remove();
-            }
-            break;
         case CREEPER:
 
             // Disabling listener if flag disabled globally
@@ -1055,14 +1046,6 @@ public class ResidenceEntityListener implements Listener {
         if (ent != null && ctype != null) {
 
             switch (ctype) {
-            case ENDER_CRYSTAL:
-                // Disabling listener if flag disabled globally
-                if (!Flags.explode.isGlobalyEnabled())
-                    break;
-                if (!perms.has(Flags.explode, perms.has(Flags.destroy, true))) {
-                    cancel = true;
-                }
-                break;
             case CREEPER:
                 // Disabling listener if flag disabled globally
                 if (!Flags.creeper.isGlobalyEnabled())
