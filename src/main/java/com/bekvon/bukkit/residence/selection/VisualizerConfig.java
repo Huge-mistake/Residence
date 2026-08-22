c9b628f44793667b78f7b06c3b080e69bb896e2a
CatTeaA <1615856447@qq.com>
2026-08-22 08:39:10 +0800

    null

--- src/main/java/com/bekvon/bukkit/residence/selection/VisualizerConfig.java
+++ src/main/java/com/bekvon/bukkit/residence/selection/VisualizerConfig.java
@@ -34,12 +34,16 @@ public class VisualizerConfig {
     private static boolean bounceAnimation;
     private static boolean enterAnimation;
 
-    private static boolean useModernVersion = true;
+    private static volatile boolean useModernVersion = true;
     private static int gridSize = 8;
     private static int updateRateByTravel = 4;
     private static double lineThickness = 0.05;
 
     public static void loadConfig(ConfigReader c) {
+
+        if (VisualizerConfig.isUseModernVersion()) {
+            CuboidDisplayManager.removeAllPlayerDisplays();
+        }
 
         c.addComment("Global.Visualizer.Use", "With this enabled player will see particle effects to mark selection boundaries");
         setShow(c.get("Global.Visualizer.Use", true));
